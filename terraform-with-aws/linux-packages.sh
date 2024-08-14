35c520bbc87f9cda3d6e35d658f200528143247c
anwar-arif <anwararif727@gmail.com>
2024-08-14 13:09:49 +0600

    add linux packages

---
+++ terraform-with-aws/linux-packages.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y --no-install-recommends \
+    apt-utils \
+    software-properties-common \
+    curl \
+    wget \
+    gnupg \
+    lsb-release \
+    vim \
+    nano \
+    less \
+    net-tools \
+    iputils-ping \
+    htop \
+    iftop \
+    iotop \
+    dstat \
+    sysstat \
+    strace \
+    lsof \
+    tcpdump \
+    traceroute \
+    ltrace \
+    bpfcc-tools \
+    perf-tools-unstable \
+    linux-tools-common \
+    linux-tools-generic \
+    procps \
+    util-linux \
+    iproute2 \
+    psmisc \
+    gdb \
+    man-db \
+    tldr \
+    sar \
+    cpuinfo \
+    meminfo \
+    diskstats \
+    stat
+
+unset DEBIAN_FRONTEND
--- terraform-with-aws/main.tf
+++ terraform-with-aws/main.tf
@@ -41,6 +41,51 @@ resource "aws_instance" "my-ubuntu" {
   key_name = aws_key_pair.my-key-pair.key_name
   security_groups = [aws_security_group.allow_ssh.name]
   
+  user_data = <<-EOF
+    export DEBIAN_FRONTEND=noninteractive
+
+    apt-get update
+    apt-get install -y --no-install-recommends \
+      apt-utils \
+      software-properties-common \
+      curl \
+      wget \
+      gnupg \
+      lsb-release \
+      vim \
+      nano \
+      less \
+      net-tools \
+      iputils-ping \
+      htop \
+      iftop \
+      iotop \
+      dstat \
+      sysstat \
+      strace \
+      lsof \
+      tcpdump \
+      traceroute \
+      ltrace \
+      bpfcc-tools \
+      perf-tools-unstable \
+      linux-tools-common \
+      linux-tools-generic \
+      procps \
+      util-linux \
+      iproute2 \
+      psmisc \
+      gdb \
+      man-db \
+      tldr \
+      sar \
+      cpuinfo \
+      meminfo \
+      diskstats \
+      stat
+    unset DEBIAN_FRONTEND
+  EOF
+  
   tags = {
     Name = "MyUbuntu"
   }
